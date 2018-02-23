454d516b39b44121d468416f5f382151876a900c
steve-sarcinella <steven.sarcinella@gmail.com>
2018-02-23 16:31:54 -0500

    updated logic, added back event loop

--- src/app.js
+++ src/app.js
@@ -2,29 +2,63 @@ require('dotenv').config();
 
 const app = require('express')();
 const bodyParser = require('body-parser');
-const request = require('request');
+const axios = require('axios');
+const _ = require('lodash');
 
 //parse application/x-www-form-urlencoded && application/json
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const eventProcessingQueue = [];
+const messagesProcessQueue = [];
+function queueMessage(requestBody) {
+  messagesProcessQueue.push(requestBody);
+}
+
+function handleMessage(messageBody) {
+  if (_.startsWith(messageBody.event.body, '/thiccify')) {
+    const message = {
+      token: process.env.SLACK_TOKEN,
+      as_user: true,
+      link_names: true,
+      text: 'THIS IS EXAMPLE TEXT, REPLACE WITH THICCEN'
+    };
+
+    message.channel = messageBody.event.channel;
+    const sendMessage = axios.post('https://slack.com/api/chat.postMessage', JSON.stringify(message));
+    sendMessage.then(postResult);
+  } else {
+    console.log('No handlers registered for that message!');
+  }
+}
+
+function runEventLoop() {
+  console.log('Running event loop');
+  while(messagesProcessQueue.length) {
+    messageBody = messagesProcessQueue.unshift();
+    handleMessage(messageBody);
+  }
+}
 
 //Define event route
 app.post('/slack_events', (req, res) => {
-  console.log(req);
   switch (req.body.type) {
     case 'url_verification': {
       res.send({ challenge: req.body.challenge });
       break;
     }
     case 'event_callback': {
+      if (req.body.token === process.env.SLACK_VERIFICATION_TOKEN) {
+        res.sendStatus(200);
+        handleMessage(req.body);
+
+      }
       if (req.body.token !== process.env.SLACK_VERIFICATION_TOKEN) {
         res.sendStatus(500);
         break;
       }
 
-      // eventProcessingQueue.push(req.body.event);
+      console.log(`Processing message: ${req.body}`);
+
       res.sendStatus(200);
     }
 
@@ -41,13 +75,5 @@ app.get('/thiccify', (req, res) => res.send('NOT IMPLEMENTED'));
 
 app.listen(process.env.PORT, () => console.log(`Speak listening on port ${process.env.PORT}`));
 
-
-function processEvents() {
-  while(eventProcessingQueue.length) {
-    let event = eventProcessingQueue.shift();
-    console.log(`Event of type ${event.type} received`);
-  }
-}
-
-//begin event loop
-setTimeout(processEvents, 1000);
+//wait 5 sec, then run every 2 sec
+setTimeout(() => setInterval(runEventLoop, 2000), 5000);
