86e88a53458d3e915001ac7273e551826833d6aa
steve-sarcinella <stevensarcinella@gmail.com>
2018-03-30 17:01:53 -0400

    misc cleanup

--- src/app.js
+++ src/app.js
@@ -5,16 +5,19 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 const _ = require('lodash');
 
+//Env vars
+const { PORT, SLACK_API_TOKEN, SLACK_VERIFICATION_TOKEN } = process.env;
+
+//Gather up all available commands
 const commandProcessors = require('./command_processors');
-const cmdProcessorMap = _.chain(commandProcessors).keyBy('cmd').mapValues('run').value();
-console.log(cmdProcessorMap);
+const commands = _.chain(commandProcessors).keyBy('cmd').mapValues('run').value();
+console.log(`Commands registered: ${_.keysIn(commands)}`);
 
 //parse application/x-www-form-urlencoded && application/json
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const { PORT, SLACK_API_TOKEN, SLACK_VERIFICATION_TOKEN } = process.env;
-
+//in case someone hits the heroku instance
 app.get('/', (req, res) => {
   res.send('<h2>MillennialSpeak</h2><p>This app pairs best with slack</p>');
 });
@@ -31,7 +34,7 @@ app.post('/millennialspeak', (req, res) => {
   console.log(`${req.body.command} called: ${JSON.stringify(req.body)}`);
   res.status(200).end();
 
-  let cmdProcessor = cmdProcessorMap[req.body.command];
+  let cmdProcessor = commands[req.body.command];
 
   if (!cmdProcessor) {
     console.log(`Command [${req.body.command}] is not valid!`);
