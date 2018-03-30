a160fddef5b850ba3e33b6fdb20973245ee99641
steve-sarcinella <stevensarcinella@gmail.com>
2018-03-30 16:10:45 -0400

    add null check for invalid commands

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const _ = require('lodash');
 const commandProcessors = require('./command_processors');
 const cmdProcessorMap = _.chain(commandProcessors).keyBy('cmd').mapValues('run').value();
 console.log(cmdProcessorMap);
+console.log(cmdProcessorMap['/clappify']('Hello everyone'));
 
 //parse application/x-www-form-urlencoded && application/json
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -31,11 +32,19 @@ app.post('/millennialspeak', (req, res) => {
   console.log(`${req.command} called: ' + JSON.stringify(req.body)`);
   res.status(200).end();
 
+  let cmdProcessor = cmdProcessorMap[req.command];
+
+  if (!cmdProcessor) {
+    console.log(`Command [${}] is not valid!`);
+    res.status(400).end();
+    return;
+  }
+
   const message = JSON.stringify({
     token: SLACK_API_TOKEN,
     as_user: true,
     channel: req.body.channel_id,
-    text: cmdProcessorMap[req.command](req.text)
+    text: cmdProcessor(req.text)
   });
 
   console.log('Chat post message: ' + message);
